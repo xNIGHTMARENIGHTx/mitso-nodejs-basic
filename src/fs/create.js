443e442a1eaf451e33c71f34a295e31cf4fe8504
xNIGHTMARENIGHTx <vqy008oj1@gmail.com>
2023-03-02 12:26:35 +0300

    feat: done create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,8 @@
+import fs from "fs/promises"
+
 const create = async () => {
-    // Write your code here 
+    await fs.writeFile("src/fs/files/fresh.txt", "I am fresh and young")
+        .catch((err) => {throw new Error("FS operation failed")})
 };
 
 await create();
