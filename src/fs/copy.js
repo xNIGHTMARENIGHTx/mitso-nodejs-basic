faa785067c9f659fac60c13f0bf3e1653a52909e
xNIGHTMARENIGHTx <vqy008oj1@gmail.com>
2023-03-02 12:15:39 +0300

    feat: copy done

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,20 @@
+import fs from "fs/promises"
+
 const copy = async () => {
-    // Write your code here 
+    let files = await fs.readdir("src/fs/files")
+        .catch((err) => {throw new Error("FS operation failed")});
+
+    let arr = new Array(files.length);
+    for(let i = 0; i < files.length; ++i){
+        arr[i] = await fs.readFile(`src/fs/files/${files[i]}`);
+    }
+    
+    await fs.mkdir("src/fs/files_copy")
+        .catch((err) => {throw new Error("FS operation failed")});
+    
+    for(let i = 0; i < files.length; ++i){
+        fs.writeFile(`src/fs/files_copy/${files[i]}`, arr[i]);
+    }
 };
 
 await copy();
