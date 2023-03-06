a0c632960d11f7c4dafd7eb7fbced88eddceb39c
xNIGHTMARENIGHTx <vqy008oj1@gmail.com>
2023-03-06 09:32:49 +0300

    feat: read done

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import fs from "fs"
+import {stdout, stderr} from "process"
+
 const read = async () => {
-    // Write your code here 
+    const streamIn = fs.createReadStream("src/streams/files/fileToRead.txt", "utf-8");
+    let data = "";
+    streamIn.on("data", chunk => data += chunk);
+    process.on("exit", code =>{
+        if(code === 0){
+            stdout.write(data);
+        }
+        else{
+            stderr.write("smt broke :(");
+        }
+    })
 };
 
 await read();
