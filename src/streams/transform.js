14faabf59295e1bba2ab6db963bcfb5562b98d12
xNIGHTMARENIGHTx <vqy008oj1@gmail.com>
2023-03-06 11:03:08 +0300

    feat: transform done

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,30 @@
+import {stdin, stdout} from "process"
+import { pipeline, Transform } from "stream"
+import fs, { write } from "fs"
+
+class myTransform extends Transform{
+    constructor(opr){
+        super(opr);
+    }
+
+    _transform(chunk, encoding, callback){
+        let data = chunk.toString().replace("\n", "").split("").reverse().join("");
+
+        this.push(data + "\n");
+        callback(null);
+    }
+}
+
 const transform = async () => {
-    // Write your code here 
+    const transformt = new myTransform();
+    pipeline(
+        stdin,
+        transformt,
+        stdout,
+        err => {
+            throw new Error("Stream operation failed");
+        }
+    )
 };
 
 await transform();
