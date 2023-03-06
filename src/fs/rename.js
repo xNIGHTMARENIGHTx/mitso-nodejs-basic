6240436ded0c9c66ae9e8eb8211e804d229f5b03
xNIGHTMARENIGHTx <vqy008oj1@gmail.com>
2023-03-06 08:29:33 +0300

    feat: rename done

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,18 @@
+import fs from "fs/promises"
+import path from "path"
+
 const rename = async () => {
-    // Write your code here 
-};
+    let data = await fs.readFile("src/fs/files/wrongFilename.txt")
+        .catch(err => {throw new Error("FS operation failed")});
+    await fs.readFile("src/fs/files/properFilename.md")
+        .catch(_err => {})
+        .then((res) =>{
+            if(res){
+                throw new Error("FS operation failed")
+            }
+        });
+    await fs.writeFile("src/fs/files/properFilename.md", data)
+        .catch(err => {throw new Error("FS operation failed")});
+};  
 
 await rename();
