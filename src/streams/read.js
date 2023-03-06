8e32ce05935028395ecd853f25574233d92f4ae0
xNIGHTMARENIGHTx <vqy008oj1@gmail.com>
2023-03-06 10:15:21 +0300

    fix: use url for path links

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,8 +1,10 @@
 import fs from "fs"
 import {stdout, stderr} from "process"
+import url from "url"
 
 const read = async () => {
-    const streamIn = fs.createReadStream("src/streams/files/fileToRead.txt", "utf-8");
+    let dirname = url.fileURLToPath(new URL(".", import.meta.url));
+    const streamIn = fs.createReadStream(`${dirname}files/fileToRead.txt`, "utf-8");
     let data = "";
     streamIn.on("data", chunk => data += chunk);
     process.on("exit", code =>{
