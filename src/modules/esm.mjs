2db5d3b4f4401637d33136a427856183ceca0d9b
xNIGHTMARENIGHTx <vqy008oj1@gmail.com>
2023-03-09 11:36:23 +0300

    feat: modules done

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,16 +1,23 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from 'path';
+import { fileURLToPath  } from 'url';
+import { release, version } from 'os';
+import { createServer as createServerHttp} from 'http';
+import('./files/c.js');
+import data1 from './files/a.json' assert { type: "json" };
+import data2 from './files/b.json' assert { type: "json" };
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = data1;
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = data2;
 }
 
 console.log(`Release ${release()}`);
@@ -33,8 +40,9 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export {
     unknownObject,
-    myServer,
-};
+    myServer
+}
+
 
