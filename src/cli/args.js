0e075d0c9d1e218ed783e6a46b361f0ec227190b
xNIGHTMARENIGHTx <vqy008oj1@gmail.com>
2023-03-11 00:44:49 +0300

    feat: cli:args done

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,20 @@
+import process from "process"
+
 const parseArgs = () => {
-    // Write your code here 
+    let args = process.argv.slice(2);
+    try{
+        if(args.length % 2 !== 0){
+            throw new Error("Args invalid");
+        }
+    }
+    catch(err){
+            console.log(err);
+            console.log("Oh no, it broke ;(");
+            return;
+        }
+    for(let i = 0; i < args.length; i+=2){
+        console.log(`${args[i]} is ${args[i+1]}`);
+    }
 };
 
 parseArgs();
