4692f414f2b4ae8160f7dbefdd0f1e28b28d708a
xNIGHTMARENIGHTx <vqy008oj1@gmail.com>
2023-03-11 00:38:28 +0300

    feat: cli:env done

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
+import process from "process"
+
 const parseEnv = () => {
-    // Write your code here 
+     let properties = Object.getOwnPropertyNames(process.env).filter(x =>{
+        return x.match("MITSO")
+     });
+     let values = new Array(properties.length);
+     for(let i = 0; i < values.length; ++i){
+        values[i] = process.env[properties[i]];
+     }
+     for(let i = 0; i < values.length; ++i){
+        console.log(`${properties[i]}=${values[i]}`)
+     }
 };
 
 parseEnv();
