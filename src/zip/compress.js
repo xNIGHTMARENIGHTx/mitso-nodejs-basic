3368e14f474e730e313d1aa8f6ca8c0cf59ed647
xNIGHTMARENIGHTx <vqy008oj1@gmail.com>
2023-03-13 09:31:51 +0300

    feat: add zip gitignore

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,27 @@
+import { promises as fs, createReadStream, createWriteStream } from "fs";
+import zlib from "zlib"
+import url from "url"
+
+const dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 const compress = async () => {
-    // Write your code here 
+    const streamIn = createReadStream(`${dirname}files/fileToCompress.txt`, "utf-8");
+    let data = '';
+    streamIn.on("data", res=>{
+        data += res;
+    })
+    const streamOut = createWriteStream(`${dirname}files/archive.gz`);
+    const transform = zlib.gzip(data, (err, buffer)=>{
+        if(!err){
+            streamOut.write(buffer);
+        }
+        else{
+            console.log(err);
+        }
+    });
+    
+
+    
 };
 
 await compress();
